dcdb7134f9f29095110c62c065f8bb08e3f5dab7
Pavel Kolomeitsev <pavelkolomeitsev1981@gmail.com>
2019-02-24 11:44:06 +0200

    Fix some bugs in SearchForTeamAdapter

--- BetProfessor/app/src/main/java/com/example/paul/betprofessor/ui/helpers/SearchForTeamAdapter.java
+++ BetProfessor/app/src/main/java/com/example/paul/betprofessor/ui/helpers/SearchForTeamAdapter.java
@@ -57,49 +57,117 @@ public class SearchForTeamAdapter extends RecyclerView.Adapter<SearchForTeamAdap
                 searchViewHolder.homeHandicap.setTextColor(Color.RED);
                 searchViewHolder.guestHandicap.setTextColor(Color.GREEN);
             }
+
+            if ((!currentResult.getHomeTeamTotal().isEmpty()) && (!currentResult.getGuestTeamTotal().isEmpty())){
+
+                if((Double.parseDouble(currentResult.getHomeResult()) > (Double.parseDouble(currentResult.getHomeTeamTotal())))){
+                    searchViewHolder.homeResult.setTextColor(Color.GREEN);
+                }else searchViewHolder.homeResult.setTextColor(Color.RED);
+
+                if ((Double.parseDouble(currentResult.getGuestResult()) > (Double.parseDouble(currentResult.getGuestTeamTotal())))){
+                    searchViewHolder.guestResult.setTextColor(Color.GREEN);
+                }else searchViewHolder.guestResult.setTextColor(Color.RED);
+            }
         }
     }
 
     private int setRightImage(String teamName) {
 
-        int imageId;
+        int imageId = R.drawable.empty_logo;
 
-        if (teamName.contains("Atlanta Hawks")) imageId = R.drawable.atlanta_hawks;
-        else if (teamName.contains("Boston Celtics")) imageId = R.drawable.boston_celtics;
-        else if (teamName.contains("Brooklyn Nets")) imageId = R.drawable.brooklyn_nets;
-        else if (teamName.contains("Charlotte Hornets")) imageId = R.drawable.charlotte_hornets;
-        else if (teamName.contains("Chicago Bulls")) imageId = R.drawable.chicago_bulls;
-        else if (teamName.contains("Cleveland Cavaliers")) imageId = R.drawable.cleveland_cavaliers;
-        else if (teamName.contains("Dallas Mavericks")) imageId = R.drawable.dallas_mavericks;
-        else if (teamName.contains("Denver Nuggets")) imageId = R.drawable.denver_nuggets;
-        else if (teamName.contains("Detroit Pistons")) imageId = R.drawable.detroit_pistons;
-        else if (teamName.contains("Golden State Warriors"))
-            imageId = R.drawable.golden_state_warriors;
-        else if (teamName.contains("Houston Rockets")) imageId = R.drawable.houston_rockets;
-        else if (teamName.contains("Indiana Pacers")) imageId = R.drawable.indiana_pacers;
-        else if (teamName.contains("Los Angeles Lakers")) imageId = R.drawable.la_lakers;
-        else if (teamName.contains("Los Angeles Clippers"))
-            imageId = R.drawable.los_angeles_clippers;
-        else if (teamName.contains("Memphis Grizzlies")) imageId = R.drawable.memphis_grizzlies;
-        else if (teamName.contains("Miami Heat")) imageId = R.drawable.miami_heat;
-        else if (teamName.contains("Milwaukee Bucks")) imageId = R.drawable.milwaukee_bucks;
-        else if (teamName.contains("Minnesota Timberwolves"))
-            imageId = R.drawable.minnesota_timberwolves;
-        else if (teamName.contains("New Orleans Pelicans"))
-            imageId = R.drawable.new_orleans_pelicans;
-        else if (teamName.contains("New York Knicks")) imageId = R.drawable.new_york_knicks;
-        else if (teamName.contains("Oklahoma City Thunder"))
-            imageId = R.drawable.oklahoma_city_thunder;
-        else if (teamName.contains("Orlando Magic")) imageId = R.drawable.orlando_magic;
-        else if (teamName.contains("Philadelphia 76ers")) imageId = R.drawable.philadelphia_76ers;
-        else if (teamName.contains("Phoenix Suns")) imageId = R.drawable.phoenix_suns;
-        else if (teamName.contains("Portland Trail Blazers"))
-            imageId = R.drawable.portland_trail_blazers;
-        else if (teamName.contains("Sacramento Kings")) imageId = R.drawable.sacramento_kings;
-        else if (teamName.contains("San Antonio Spurs")) imageId = R.drawable.san_antonio_spurs;
-        else if (teamName.contains("Toronto Raptors")) imageId = R.drawable.toronto_raptors;
-        else if (teamName.contains("Utah Jazz")) imageId = R.drawable.utah_jazz;
-        else imageId = R.drawable.washington_wizards;
+        switch (teamName){
+
+            case "Atlanta Hawks":
+                imageId = R.drawable.atlanta_hawks;
+                break;
+            case "Boston Celtics":
+                imageId = R.drawable.boston_celtics;
+                break;
+            case "Brooklyn Nets":
+                imageId = R.drawable.brooklyn_nets;
+                break;
+            case "Charlotte Hornets":
+                imageId = R.drawable.charlotte_hornets;
+                break;
+            case "Chicago Bulls":
+                imageId = R.drawable.chicago_bulls;
+                break;
+            case "Cleveland Cavaliers":
+                imageId = R.drawable.cleveland_cavaliers;
+                break;
+            case "Dallas Mavericks":
+                imageId = R.drawable.dallas_mavericks;
+                break;
+            case "Denver Nuggets":
+                imageId = R.drawable.denver_nuggets;
+                break;
+            case "Detroit Pistons":
+                imageId = R.drawable.detroit_pistons;
+                break;
+            case "Golden State Warriors":
+                imageId = R.drawable.golden_state_warriors;
+                break;
+            case "Houston Rockets":
+                imageId = R.drawable.houston_rockets;
+                break;
+            case "Indiana Pacers":
+                imageId = R.drawable.indiana_pacers;
+                break;
+            case "Los Angeles Lakers":
+                imageId = R.drawable.la_lakers;
+                break;
+            case "Los Angeles Clippers":
+                imageId = R.drawable.los_angeles_clippers;
+                break;
+            case "Memphis Grizzlies":
+                imageId = R.drawable.memphis_grizzlies;
+                break;
+            case "Miami Heat":
+                imageId = R.drawable.miami_heat;
+                break;
+            case "Milwaukee Bucks":
+                imageId = R.drawable.milwaukee_bucks;
+                break;
+            case "Minnesota Timberwolves":
+                imageId = R.drawable.minnesota_timberwolves;
+                break;
+            case "New Orleans Pelicans":
+                imageId = R.drawable.new_orleans_pelicans;
+                break;
+            case "New York Knicks":
+                imageId = R.drawable.new_york_knicks;
+                break;
+            case "Oklahoma City Thunder":
+                imageId = R.drawable.oklahoma_city_thunder;
+                break;
+            case "Orlando Magic":
+                imageId = R.drawable.orlando_magic;
+                break;
+            case "Philadelphia 76ers":
+                imageId = R.drawable.philadelphia_76ers;
+                break;
+            case "Phoenix Suns":
+                imageId = R.drawable.phoenix_suns;
+                break;
+            case "Portland Trail Blazers":
+                imageId = R.drawable.portland_trail_blazers;
+                break;
+            case "Sacramento Kings":
+                imageId = R.drawable.sacramento_kings;
+                break;
+            case "San Antonio Spurs":
+                imageId = R.drawable.san_antonio_spurs;
+                break;
+            case "Toronto Raptors":
+                imageId = R.drawable.toronto_raptors;
+                break;
+            case "Utah Jazz":
+                imageId = R.drawable.utah_jazz;
+                break;
+            case "Washington Wizards":
+                imageId = R.drawable.washington_wizards;
+                break;
+        }
 
         return imageId;
     }
